afaebd5711c705536b0e09dfa09bbf6de1736b1a
etmillin <emillin@browsium.com>
2013-03-09 13:41:55 -0500

    Added example

---
+++ examples/example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using PacParserDotNet;
+
+
+namespace pacparser.net.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// call pacparser.net piece-by-piece
+			if(PacParser.Initialize() &&
+				PacParser.ParsePacFile("proxy.pac"))
+			{
+				var proxy = PacParser.FindProxy("http://news.manugarg.com", "news.manugarg.com");
+				Console.WriteLine("FindProxy returned: " + proxy);
+
+				PacParser.Cleanup();
+			}
+
+            while (String.IsNullOrEmpty(Console.ReadLine()))
+                ;
+        }
+    }
+
+}
